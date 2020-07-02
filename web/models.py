0cca78b0112bb72fb0b0dce34a32d9e893a98501
fbugraaydin <fuatbugraaydin@gmail.com>
2020-07-02 17:05:14 +0300

    creates table migrations.

--- web/models.py
+++ web/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Link(models.Model):
+    id = models.AutoField(primary_key=True)
+    link = models.TextField(unique=True)
+
+
+class Statistic(models.Model):
+    date = models.TextField(),
+    average_amount = models.FloatField()
+    link_id = models.IntegerField()
