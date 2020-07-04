3b3d2ad6a73c16ed6daa332cd8a9f12e3a41efd8
fbugraaydin <fuatbugraaydin@gmail.com>
2020-07-04 18:22:44 +0300

    Fixes deployment bug.

--- carpriceanalyst/settings.py
+++ carpriceanalyst/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -111,11 +112,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-# Heroku: Update database configuration from $DATABASE_URL.
-import dj_database_url
-
-db_from_env = dj_database_url.config(conn_max_age=500)
-DATABASES['default'].update(db_from_env)
+django_on_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
