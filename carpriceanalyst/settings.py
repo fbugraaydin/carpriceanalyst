5130c1acc0f795228abdb882fac6d7214821195e
fbugraaydin <fuatbugraaydin@gmail.com>
2020-07-04 18:30:38 +0300

    Fixes deployment bug.

--- carpriceanalyst/settings.py
+++ carpriceanalyst/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import django_on_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -110,6 +111,8 @@ USE_I18N = True
 USE_L10N = True
 
 USE_TZ = True
+
+django_on_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
