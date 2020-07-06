316860dc163864671b69163ced0ae24e601c69e7
fbugraaydin <fuatbugraaydin@gmail.com>
2020-07-06 23:09:26 +0300

    Adds heroku specific selenium impls.

--- web/crawler.py
+++ web/crawler.py
@@ -15,15 +15,32 @@ logger.addHandler(logging.StreamHandler())
 
 
 def get_page_source(url):
+    logger.info('Getting page source from :' + url)
     try:
         chrome_options = webdriver.ChromeOptions()
+        chrome_options.binary_location = os.environ.get("GOOGLE_CHROME_BIN")
         chrome_options.add_argument("--headless")
         chrome_options.add_argument("--no-sandbox")
         chrome_options.add_argument("--disable-dev-shm-usage")
         chrome_options.add_argument("--disable-gpu")
         chrome_options.add_argument("--window-size=1920,1080")
+        #chrome_options.add_argument(f'user-agent={userAgent}')
+        #chrome_options.add_experimental_option("excludeSwitches", ["enable-automation"])
+        #chrome_options.add_experimental_option('useAutomationExtension', False)
+        chrome_options.add_argument("--disable-blink-features")
+        chrome_options.add_argument("--disable-blink-features=AutomationControlled")
+        chrome_options.add_argument('ignore-certificate-errors')
 
-        browser = webdriver.Chrome(executable_path=chrome_driver_abs_path, chrome_options=chrome_options)
+        browser = webdriver.Chrome(executable_path=os.environ.get("CHROMEDRIVER_PATH"), chrome_options=chrome_options)
+        #browser.execute_cdp_cmd('Network.setUserAgentOverride', {"userAgent": 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.53 Safari/537.36'})
+        # browser.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
+        #     "source": """
+        #     Object.defineProperty(navigator, 'webdriver', {
+        #       get: () => undefined
+        #     })
+        #   """
+        # })
+
         browser.get(url)
         html = browser.page_source
         time.sleep(2)
